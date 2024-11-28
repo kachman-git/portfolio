5a579dee64b478af62e41a868cf03ef72779ce52
kachman-git <onyekachiokpala67@gmail.com>
2024-11-28 04:40:07 +0000

    First Commit

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,8 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Contact", link: "#contact" },
   { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
 ];
 
 export const gridItems = [
